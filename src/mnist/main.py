60a80b01eac46fdb6102b36f0b0c5c55492411b2
dMario24 <data.mario24@gmail.com>
2024-09-20 15:26:24 +0900

    common

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -1,13 +1,25 @@
 from typing import Annotated
-
 from fastapi import FastAPI, File, UploadFile
+import os
+import pymysql.cursors
+import json
 
 app = FastAPI()
 
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+@app.get("/files")
+async def file_list():
+    conn = pymysql.connect(host='172.18.0.1', port = 53306,
+                            user = 'mnist', password = '1234',
+                            database = 'mnistdb',
+                            cursorclass=pymysql.cursors.DictCursor)
+    with conn:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM image_processing WHERE prediction_time IS NULL ORDER BY num"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+    return result
 
 
 @app.post("/uploadfile/")
@@ -15,27 +27,69 @@ async def create_upload_file(file: UploadFile):
     # 파일 저장
     img = await file.read()
     file_name = file.filename
+    file_ext = file.content_type.split('/')[-1]
+
     # 디렉토리가 없으면 오류, 코드에서 확인 및 만들기 추가
-    upload_dir = "./photo"
-    import os
-    file_full_path = os.path.join(upload_dir, file_name)
+    upload_dir = "/home/diginori/code/mnist/img"
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    import uuid
+    file_full_path = os.path.join(upload_dir, 
+            f'{uuid.uuid4()}.{file_ext}')
 
     with open(file_full_path, "wb") as f:
         f.write(img)
 
     # 파일 저장 경로 DB INSERT
-    # tablename : image_processing
-    # 컬럼 정보 : num (초기 인서트, 자동 증가)
-    # 컬럼 정보 : 파일이름, 파일경로, 요청시간(초기 인서트), 요청사용자(n00)
-    # 컬럼 정보 : 예측모델, 예측결과, 예측시간(추후 업데이트)
+    # INSERT INTO image_processing(file_name, file_path,
+    # request_time, request_user)
+    # VALUES('123.png', '/a/b/c/123.png', '2024-09-20 10:20:11', 'n99');
+    sql = "INSERT INTO image_processing(file_name, file_path, request_time, request_user) VALUES(%s, %s, %s, %s)"
+    conn = pymysql.connect(host='172.18.0.1', port = 53306,
+                            user = 'mnist', password = '1234',
+                            database = 'mnistdb',
+                            cursorclass=pymysql.cursors.DictCursor)
+    
+    # https://pypi.org/project/pytz/ 'Asia/Seoul'
+    import jigeum.seoul
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql, (file_name,
+                                file_full_path,
+                                jigeum.seoul.now(),
+                                'n99'))
+        conn.commit()
+    
     return {
             "filename": file.filename,
             "content_type": file.content_type,
             "file_full_path": file_full_path
            }
 
+@app.get("/all")
+def all():
+    from mnist.db import select
+    sql = "SELECT * FROM image_processing"
+    result = select(query=sql, size=-1)
+    return result
 
+@app.get("/one")
+def one():
+    from mnist.db import select
+    sql = """SELECT * FROM image_processing 
+    WHERE prediction_time IS NULL ORDER BY num LIMIT 1"""
+    result = select(query=sql, size=1)
+    return result[0]
 
+@app.get("/many/")
+def many(size: int = -1):
+    from mnist.db import get_conn
+    sql = "SELECT * FROM image_processing WHERE prediction_time IS NULL ORDER BY num"
+    conn = get_conn()
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            result = cursor.fetchmany(size)
 
-
+    return result
 
