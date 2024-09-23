a29206395ab26665ae4045f5ecaeddd94af9caa4
dMario24 <data.mario24@gmail.com>
2024-09-23 17:25:21 +0900

    0.3.5

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -23,7 +23,7 @@ async def file_list():
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile, insert_loop: int = 1):
     # 파일 저장
     img = await file.read()
     file_name = file.filename
@@ -43,7 +43,10 @@ async def create_upload_file(file: UploadFile):
     sql = "INSERT INTO image_processing(file_name, file_path, request_time, request_user) VALUES(%s, %s, %s, %s)"
     import jigeum.seoul 
     from mnist.db import dml
-    insert_row = dml(sql, file_name, file_full_path, jigeum.seoul.now(), 'n99')
+
+    insert_row = 0
+    for _ in range(insert_loop):
+        insert_row = dml(sql, file_name, file_full_path, jigeum.seoul.now(), 'n99')
     
     return {
             "filename": file.filename,
