cc770f64552f5bf59ad69d8011381ac0c204b8b4
dMario24 <data.mario24@gmail.com>
2024-09-23 14:36:33 +0900

    02:36

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -1,14 +1,46 @@
 import jigeum.seoul
+import mnist.db import select, dml
+import random
+
+def get_job_img_task():
+   sql = """
+   SELECT 
+    num, file_name, file_path
+   FROM image_processing
+   WHERE prediction_result IS NULL
+   ORDER BY num -- 가장 오래된 요청
+   LIMIT 1 -- 하나씩
+   """
+   r = select(sql, 1)
+   return r[0]
+
+def prediction(file_path, num):
+    sql = """UPDATE image_processing
+    SET prediction_result=%s,
+        prediction_model='n77',
+        prediction_time=%s
+    WHERE num=%s
+    """
+    presult = random.randint(0, 9)
+    dml(sql, presult, jigeum.seoul.now(), num)
+
+    return presult
+
 
 def run():
   """image_processing 테이블을 읽어서 가장 오래된 요청 하나씩을 처리"""
   
   # STEP 1
   # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 갖여오기
+  job = get_job_img_task()
+  num = job['num']
+  file_name = job['file_name']
+  file_path = job['file_path']
 
   # STEP 2
   # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
   # 동시에 prediction_model, prediction_time 도 업데이트
+  presutl = prediction(file_path, num) 
 
   # STEP 3
   # LINE 으로 처리 결과 전송
