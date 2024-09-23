1754cb98f4af53dac0e1f5cff11d4beaaa7eb8a6
dMario24 <data.mario24@gmail.com>
2024-09-23 15:21:00 +0900

    job is None

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -12,7 +12,11 @@ def get_job_img_task():
    LIMIT 1 -- 하나씩
    """
    r = select(sql, 1)
-   return r[0]
+
+   if len(r) > 0:
+       return r[0]
+   else:
+       return None
 
 def prediction(file_path, num):
     sql = """UPDATE image_processing
@@ -33,6 +37,11 @@ def run():
   # STEP 1
   # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 갖여오기
   job = get_job_img_task()
+    
+  if job is None:
+      print(f"{jigeum.seoul.now()} - jos is None")
+      return 
+
   num = job['num']
   file_name = job['file_name']
   file_path = job['file_path']
--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,5 +1,10 @@
-from mnist.worker import prediction
+from mnist.worker import prediction, get_job_img_task
 
 def test_prediction():
-    r = prediction(file_path='/a/b/c/d.png', num=1)
+    r = prediction(file_path='/a/b/c/d.png', num=2)
     assert r in range(10)
+
+
+def test_get_job_img_task():
+    r = get_job_img_task()
+    assert True
